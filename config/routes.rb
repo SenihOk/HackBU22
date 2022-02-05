6a6c3f8972e4f347c938c29e9fc0745162038c46
Senih Okuyucu <sokuyuc1@binghamton.edu>
2022-02-05 17:40:19 -0500

    forgot a hashtag

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   root to: 'welcome#index'
 
-  get "get-started", to: "get-started"
+  get "get-started", to: "get-started#getstarted"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
